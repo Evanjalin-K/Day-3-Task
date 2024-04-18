fa1151c0ce0d6353722d260545631432e8f532d8
Evanjalin-K <jojaev510@gmail.com>
2024-04-18 23:22:19 +0530

    Initial Commit

---
+++ ComparingJason/ComparingJason.js
@@ -0,0 +1,17 @@
+const isEqual = (obj1, obj2) => {
+    let obj1keys = Object.keys(obj1);
+    let obj2keys = Object.keys(obj2);
+    if (obj1keys.length !== obj2keys.length) {
+        return false;
+    }
+    for (let objkey of obj1keys) {
+        if(obj1[objkey] !== obj2[objkey]){
+            return false;
+        }
+    }
+            return true;
+}
+    
+let obj1 = {name: "Person", age:5};
+let obj2 = {age:5, name: "Person"};
+console.log(isEqual(obj1, obj2));
